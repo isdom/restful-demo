3b8537c5fb09df59d56d056b30f46d112a453a40
ming.ma <isdom.maming@gmail.com>
2015-09-10 17:48:32 +0800

    注解 POST 方式 请求

--- src/main/java/org/jocean/restfuldemo/bean/outbound/OutboundRequest.java
+++ src/main/java/org/jocean/restfuldemo/bean/outbound/OutboundRequest.java
@@ -3,8 +3,11 @@
  */
 package org.jocean.restfuldemo.bean.outbound;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+
+import org.jocean.idiom.AnnotationWrapper;
 
 import com.alibaba.fastjson.annotation.JSONField;
 
@@ -14,6 +17,7 @@ import com.alibaba.fastjson.annotation.JSONField;
  *
  */
 @Path("/boards")
+@AnnotationWrapper(POST.class)  // 用 POST HTTP Method 请求
 public class OutboundRequest {
     
     @JSONField(name="name")
