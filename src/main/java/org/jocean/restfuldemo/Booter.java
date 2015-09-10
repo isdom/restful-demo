629ee4b6ce278deecf25a8271b802234f3e17093
ming.ma <isdom.maming@gmail.com>
2015-09-10 12:01:30 +0800

    添加 自动加载 /lib 下所有 jar 的功能, 启动主类变更为 Booter

---
+++ src/main/java/org/jocean/restfuldemo/Booter.java
@@ -0,0 +1,22 @@
+package org.jocean.restfuldemo;
+
+
+public class Booter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String[] extJars = JVMUtil.addAllJarsToClassPath(System.getProperty("user.dir") + "/lib");
+        for (String jarName : extJars) {
+            System.out.println("add path [" + jarName + "]");
+        }
+
+        try {
+            Main.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
