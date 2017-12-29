9948969c119032437ecfa6b95dbf18afaf31e04e
ming.ma <isdom.maming@gmail.com>
2017-12-29 23:26:15 +0800

    完善 流式演示接口 /newrest/stream

--- src/main/java/org/jocean/restfuldemo/bll2/DemoResource.java
+++ src/main/java/org/jocean/restfuldemo/bll2/DemoResource.java
@@ -1,5 +1,9 @@
 package org.jocean.restfuldemo.bll2;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -13,9 +17,9 @@ import javax.ws.rs.QueryParam;
 import org.jocean.http.FullMessage;
 import org.jocean.http.MessageBody;
 import org.jocean.http.WritePolicy;
-import org.jocean.http.WritePolicy.Outboundable;
 import org.jocean.http.util.RxNettys;
 import org.jocean.idiom.DisposableWrapper;
+import org.jocean.idiom.DisposableWrapperUtil;
 import org.jocean.netty.BlobRepo;
 import org.jocean.restfuldemo.bean.DemoRequest;
 import org.jocean.svr.MessageDecoder;
@@ -28,7 +32,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
@@ -40,6 +45,7 @@ import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Func1;
@@ -53,17 +59,43 @@ public class DemoResource {
     
     @Path("stream")
     public Object bigresp(final WritePolicyAware writePolicyAware) {
-        final AtomicReference<Outboundable> ref = new AtomicReference<>();
+        final AtomicInteger count = new AtomicInteger(0);
+        final Func1<Queue<String>, Boolean> fillContent = content -> {
+            if (count.get() < 100) {
+                LOG.debug("re-fill content");
+                count.incrementAndGet();
+                for (int idx = 1; idx < 10000; idx++) {
+                    content.add(Integer.toString(idx) + ".");
+                }
+                return true;
+            } else {
+                return false;
+            }
+        };
+        
+        final Queue<String> content = new LinkedList<>();
+        final AtomicReference<Object> lastRef = new AtomicReference<>(null);
+        
+        final AtomicReference<Subscriber<? super DisposableWrapper<ByteBuf>>> subref = 
+                new AtomicReference<>();
+        
         writePolicyAware.setWritePolicy(new WritePolicy() {
-
             @Override
             public void applyTo(final Outboundable outboundable) {
-                ref.set(outboundable);
                 outboundable.setFlushPerWrite(true);
+                outboundable.sended().subscribe(obj -> {
+                    LOG.debug("sended :{}", obj);
+                    DisposableWrapperUtil.dispose(obj);
+                    if (obj == lastRef.get()) {
+                        // the last of current send batch
+                        final Subscriber<? super DisposableWrapper<ByteBuf>> subscriber = subref.get();
+                        buildContentAndSend(content, fillContent, lastRef, subscriber);
+                    }});
             }});
-        final AtomicInteger idx = new AtomicInteger(0);
+        
         return Observable.just(new FullMessage() {
 
+            @SuppressWarnings("unchecked")
             @Override
             public <M extends HttpMessage> M message() {
                 final HttpResponse resp = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
@@ -88,38 +120,73 @@ public class DemoResource {
 
                     @Override
                     public Observable<? extends DisposableWrapper<ByteBuf>> content() {
-                        final AtomicReference<Subscriber> subref = new AtomicReference<>();
-                        ref.get().sended().subscribe(obj -> {
-                            LOG.debug("sended :{}", obj);
-                            if (obj instanceof DisposableWrapper) {
-                                final DisposableWrapper<ByteBuf> dwb = (DisposableWrapper<ByteBuf>) obj;
-                                dwb.dispose();
-                                if (idx.incrementAndGet() < 100) {
-                                    subref.get().onNext(buildBuf(idx.getAndIncrement()));
-                                } else {
-                                    subref.get().onCompleted();
-                                }
-                            }
-                        });
-                        return Observable.unsafeCreate(subscriber -> { 
+                        return Observable.unsafeCreate(subscriber -> {
                             subref.set(subscriber);
-                            subscriber.onNext(buildBuf(idx.getAndIncrement()));
+                            buildContentAndSend(content, fillContent, lastRef, subscriber);
                         });
-//                                Observable.concat(
-//                                Observable.just(buildBuf(idx)),
-//                                ref.get().sended().flatMap(obj -> {
-//                                    final DisposableWrapper<ByteBuf> dwb = (DisposableWrapper<ByteBuf>) obj;
-//                                    dwb.dispose();
-//                                    if (idx.incrementAndGet() < 100) {
-//                                        return Observable.just(buildBuf(idx));
-//                                    } else {
-//                                        return Observable.empty();
-//                                    }
-//                                }));
                     }});
             }});
     }
-    
+
+    private void buildContentAndSend(
+            final Queue<String> content, 
+            final Func1<Queue<String>, Boolean> fillContent,
+            final AtomicReference<Object> lastRef, 
+            final Subscriber<? super DisposableWrapper<ByteBuf>> subscriber) {
+        if (content.isEmpty()) {
+            if (!fillContent.call(content)) {
+                subscriber.onCompleted();
+                LOG.debug("content onCompleted");
+                return;
+            }
+        }
+        sendContentAndMark(content, lastRef, subscriber);
+    }
+
+    private void sendContentAndMark(
+            final Queue<String> content,
+            final AtomicReference<Object> lastRef,
+            final Subscriber<? super DisposableWrapper<ByteBuf>> subscriber) {
+        LOG.debug("content's count: {} with first element: {}", content.size(), content.peek());
+        final List<DisposableWrapper<ByteBuf>> dwbs = toDWBS(content, 5);
+        final DisposableWrapper<ByteBuf> last = dwbs.get(dwbs.size()-1);
+        LOG.debug("dwbs size: {}, last element: {}", dwbs.size(), last);
+        lastRef.set(last);
+        for (DisposableWrapper<ByteBuf> dwb : dwbs) {
+            subscriber.onNext(dwb);
+        }
+        dwbs.clear();
+    }
+
+    private final List<DisposableWrapper<ByteBuf>> toDWBS(final Queue<String> ids, final int maxBufCount) {
+        final List<DisposableWrapper<ByteBuf>> dwbs = new ArrayList<>(maxBufCount);
+        final ByteBufAllocator allocator = PooledByteBufAllocator.DEFAULT;
+        
+        ByteBuf buf = allocator.buffer(8192, 8192);
+        String line = ids.peek();
+        
+        while (null != line) {
+            final byte[] bytes = line.getBytes(CharsetUtil.UTF_8);
+            if (bytes.length <= buf.maxWritableBytes()) {
+                buf.writeBytes(bytes);
+                ids.remove();
+            } else {
+                dwbs.add(RxNettys.wrap4release(buf));
+                if (dwbs.size() >= maxBufCount) {
+                    return dwbs;
+                }
+                buf = allocator.buffer(8192, 8192);
+            }
+            line = ids.peek();
+        }
+        if (buf.readableBytes() > 0) {
+            dwbs.add(RxNettys.wrap4release(buf));
+        } else {
+            buf.release();
+        }
+        return dwbs;
+    }
+
     @Path("hello")
     @OPTIONS
     @POST
@@ -214,11 +281,6 @@ public class DemoResource {
 //        });
 //    }
     
-    private DisposableWrapper<ByteBuf> buildBuf(final int idx) {
-        return RxNettys
-                .wrap4release(Unpooled.wrappedBuffer(Integer.toString(idx).getBytes()));
-    }
-
     @Inject
     private BlobRepo _blobRepo;
     
