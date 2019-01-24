21839e9147d3a5e66b4301645bdb0a470fe5b194
ming.ma <isdom@qq.com>
2019-01-24 15:17:50 +0800

    测试 Unix Domain Socket

---
+++ src/main/java/org/jocean/restfuldemo/uds/EchoServerHandler.java
@@ -0,0 +1,29 @@
+package org.jocean.restfuldemo.uds;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handler implementation for the echo server.
+ */
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, final Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(final ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/org/jocean/restfuldemo/uds/UDSDemo.java
@@ -0,0 +1,54 @@
+package org.jocean.restfuldemo.uds;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerDomainSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.unix.DomainSocketAddress;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+public class UDSDemo {
+
+    static final EventLoopGroup EPOLL_BOSS_GROUP =
+            new EpollEventLoopGroup(1, new DefaultThreadFactory("epoll-boss", false));
+
+    static final EventLoopGroup EPOLL_WORKER_GROUP =
+            new EpollEventLoopGroup(1, new DefaultThreadFactory("epoll-worker", false));
+
+    Channel _bindChannel;
+
+    public void start() throws InterruptedException {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+
+        final ServerBootstrap sb = new ServerBootstrap().group(EPOLL_BOSS_GROUP, EPOLL_WORKER_GROUP)
+                        .channel(EpollServerDomainSocketChannel.class)
+                        .option(ChannelOption.SO_BACKLOG, 100)
+                        .handler(new LoggingHandler(LogLevel.INFO))
+                        .childHandler(new ChannelInitializer<SocketChannel>() {
+                            @Override
+                            protected void initChannel(final SocketChannel channel) throws Exception {
+                                final ChannelPipeline p = channel.pipeline();
+                                p.addLast(serverHandler);
+                            }});
+        final DomainSocketAddress localAddress = new DomainSocketAddress("./demo.socket");
+
+        final ChannelFuture future = sb.bind(localAddress);
+        future.sync();
+
+        _bindChannel = future.channel();
+    }
+
+    public void stop() throws InterruptedException {
+        if (_bindChannel != null) {
+            _bindChannel.close().sync();
+        }
+    }
+}
