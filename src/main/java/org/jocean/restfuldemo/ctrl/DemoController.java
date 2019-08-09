2b214481b0a6d742e00f2ecdf42c71ac853b0dd7
ming.ma <isdom@qq.com>
2019-08-09 15:07:54 +0800

    演示 阿里云智能语音交互 中的一句话识别 API: http://nls-gateway.cn-shanghai.aliyuncs.com/stream/v1/asr

--- src/main/java/org/jocean/restfuldemo/ctrl/DemoController.java
+++ src/main/java/org/jocean/restfuldemo/ctrl/DemoController.java
@@ -30,6 +30,7 @@ import org.jocean.aliyun.ccs.CCSChatAPI;
 import org.jocean.aliyun.ccs.CCSChatUtil;
 import org.jocean.aliyun.ecs.MetadataAPI;
 import org.jocean.aliyun.nls.NlsAPI;
+import org.jocean.aliyun.nls.NlsAPI.AsrResponse;
 import org.jocean.aliyun.nls.NlsAPI.CreateTokenResponse;
 import org.jocean.aliyun.oss.BlobRepoOverOSS;
 import org.jocean.http.ByteBufSlice;
@@ -100,6 +101,14 @@ import rx.Observable.Transformer;
 @Scope("singleton")
 public class DemoController implements MBeanRegisterAware {
     private static final Logger LOG = LoggerFactory.getLogger(DemoController.class);
+
+    @Path("nlsasr")
+    @POST
+    public Observable<AsrResponse> nlsasr(final RpcExecutor executor,
+            final Observable<MessageBody> getbody) {
+        return getbody.flatMap(body ->
+            executor.execute(_finder.find(NlsAPI.class).map(api -> api.streamAsrV1(body) )));
+    }
 
     @Path("nlstoken")
     @GET
