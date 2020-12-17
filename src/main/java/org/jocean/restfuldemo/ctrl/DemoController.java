a7b69724dc5a46561366fe3905177cc0c98cf33e
ming.ma <isdom@qq.com>
2020-12-17 11:08:18 +0800

    从 DemoController 中移除 OSS 相关示例

--- src/main/java/org/jocean/restfuldemo/ctrl/DemoController.java
+++ src/main/java/org/jocean/restfuldemo/ctrl/DemoController.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -32,7 +31,6 @@ import org.jocean.aliyun.ccs.CCSChatUtil;
 import org.jocean.aliyun.ecs.MetadataAPI;
 import org.jocean.aliyun.ivision.IvisionAPI;
 import org.jocean.aliyun.oss.BlobRepoOverOSS;
-import org.jocean.aliyun.oss.OssAPI;
 import org.jocean.aliyun.sign.AliyunSigner;
 import org.jocean.aliyun.sign.Signer4OSS;
 import org.jocean.aliyun.sign.SignerV1;
@@ -107,147 +105,6 @@ import rx.functions.Action1;
 @Scope("prototype")
 public class DemoController /* implements MBeanRegisterAware */ {
     private static final Logger LOG = LoggerFactory.getLogger(DemoController.class);
-
-    @Value("${oss.endpoint}")
-    private String _ossEndpoint;
-
-    @Value("${oss.bucket}")
-    private String _ossBucket;
-
-    @Value("${upload.path}")
-    private String _uploadPath;
-
-    @Path("oss/getslink")
-    public Observable<FullMessage<HttpResponse>> getslink(
-            @QueryParam("symlink") final String symlink,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss
-            ) {
-        return oss.getSymlink().bucket(_ossBucket).endpoint(_ossEndpoint).symlinkObject(symlink).call();
-    }
-
-    @Path("oss/putslink")
-    public Observable<FullMessage<HttpResponse>> putslink(
-            @QueryParam("symlink") final String symlink,
-            @QueryParam("target") final String target,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss
-            ) {
-        return oss.putSymlink()
-            .bucket(_ossBucket)
-            .endpoint(_ossEndpoint)
-            .symlinkObject(symlink)
-            .targetObject(target)
-            .call();
-    }
-
-    @Path("oss/delobj")
-    public Observable<FullMessage<HttpResponse>> deleteObject(
-            @QueryParam("obj") final String object,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss) {
-        return oss.deleteObject()
-            .bucket(_ossBucket)
-            .endpoint(_ossEndpoint)
-            .object(object)
-            .call();
-    }
-
-    @Path("oss/copyobj")
-    public Observable<FullMessage<HttpResponse>> copyObject(
-            @QueryParam("dest") final String dest,
-            @QueryParam("sourcePath") final String sourcePath,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss) {
-        return oss.copyObject()
-            .bucket(_ossBucket)
-            .endpoint(_ossEndpoint)
-            .destObject(dest)
-            .source(sourcePath)
-            .call();
-    }
-
-    @Path("oss/listobj")
-    public Observable<String> list(
-            @QueryParam("prefix") final String prefix,
-            @QueryParam("marker") final String marker,
-            @QueryParam("delimiter") final String delimiter,
-            @QueryParam("encodingType") final String encodingType,
-            @QueryParam("maxKeys") final String maxKeys,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss) {
-        return oss.listObjects()
-            .bucket(_ossBucket)
-            .endpoint(_ossEndpoint)
-            .prefix(prefix)
-            .marker(marker)
-            .delimiter(delimiter)
-            .encodingType(encodingType)
-            .maxKeys(maxKeys)
-            .call()
-            .map(listing -> listing.toString());
-    }
-
-    @Path("oss/meta")
-    public Observable<String> ossmeta(@QueryParam("obj") final String objname,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss) {
-        return oss.getObjectMeta()
-            .bucket(_ossBucket)
-            .endpoint(_ossEndpoint)
-            .object(objname)
-            .call()
-            .map( fullmsg -> fullmsg.message().headers().toString() );
-    }
-
-    @Path("oss/upload")
-    @POST
-    public Observable<Object> uploadNew(
-            final HttpRequest request,
-            final Observable<MessageBody> getbody,
-            @RpcFacade("this.alisign_sts_oss()") final OssAPI oss) {
-        return handle100Continue(request)
-                .concatWith(oss.putObject()
-                        .bucket(_ossBucket)
-                        .endpoint(_ossEndpoint)
-                        .object(_uploadPath + "/" + UUID.randomUUID().toString().replaceAll("-", ""))
-                        .body(getbody)
-                        .call());
-    }
-
-    /*
-    private Transformer<Interact, PutObjectResult> putObject(
-            final String bucketName,
-            final String objname,
-            final Observable<MessageBody> getbody) {
-        return interacts -> interacts.flatMap( interact -> interact.name("oss.putObject").method(HttpMethod.PUT)
-                .uri(uri4bucket(bucketName))
-                .path("/" + objname)
-                .body(getbody)
-                .response()
-                .<HttpResponse>flatMap(resp -> {
-                    // https://help.aliyun.com/document_detail/32005.html?spm=a2c4g.11186623.6.1090.DeJEv5
-                    final String contentType = resp.message().headers().get(HttpHeaderNames.CONTENT_TYPE);
-                    if (null != contentType && contentType.startsWith("application/xml")) {
-                        return OSSUtil.extractAndReturnOSSError(resp, "putObject error");
-                    } else {
-                        return Observable.just(resp.message());
-                    }
-                })
-                .map(resp -> resp.headers().get(HttpHeaderNames.ETAG)).<PutObjectResult>map(etag -> {
-                    final String unquotes_etag = etag.replaceAll("\"", "");
-                    return new PutObjectResult() {
-                        @Override
-                        public String objectName() {
-                            return objname;
-                        }
-
-                        @Override
-                        public String etag() {
-                            return unquotes_etag;
-                        }
-                    };
-                }));
-    }
-
-    private String uri4bucket(final String bucketName) {
-        return "http://" + bucketName + "." + _ossEndpoint;
-    }
-    */
 
     @RpcFacade
     MetadataAPI.STSTokenBuilder  getststoken;
