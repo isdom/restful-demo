97f766cc3f53aa4dab0d729800f13b72c90c1353
ming.ma <isdom@qq.com>
2021-04-17 22:06:41 +0800

    注释用 wget 验证 basic/stream; 新增 StreamDemo 专用于使用 WithSubscriber 实现流式响应

--- src/main/java/org/jocean/restfuldemo/ctrl/StreamDemo.java
+++ src/main/java/org/jocean/restfuldemo/ctrl/StreamDemo.java
@@ -1,7 +1,10 @@
 package org.jocean.restfuldemo.ctrl;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.HeaderParam;
@@ -11,6 +14,7 @@ import org.jocean.http.WriteCtrl;
 import org.jocean.idiom.ExceptionUtils;
 import org.jocean.svr.MutableResponseBean;
 import org.jocean.svr.ResponseUtil;
+import org.jocean.svr.WithSubscriber;
 import org.jocean.svr.annotation.HandleError;
 import org.jocean.svr.annotation.OnError;
 import org.slf4j.Logger;
@@ -18,20 +22,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.google.common.base.Charsets;
+
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.cookie.Cookie;
 import io.netty.handler.codec.http.cookie.DefaultCookie;
 import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action2;
 
 
 @Path("/newrest/")
 @Controller
 @Scope("prototype")
-public class BasicDemo {
-    private static final Logger LOG = LoggerFactory.getLogger(BasicDemo.class);
+public class StreamDemo {
+    private static final Logger LOG = LoggerFactory.getLogger(StreamDemo.class);
 
     @HandleError(Exception.class)
     String handleAllError(final HttpRequest req, final Exception e) {
@@ -39,27 +46,38 @@ public class BasicDemo {
         return "error when " + req.uri() + "{\n" + ExceptionUtils.exception2detail(e) + "\n}";
     }
 
-    @Path("basic/testcookie1")
+    @Path("stream/string1")
     @OnError({
         "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
         ,"this.handleAllError"
         })
-    public Object testcookie1(@CookieParam("hello") final String hello,
-            final WriteCtrl writeCtrl) {
-        if (null == hello || hello.isEmpty()) {
-            writeCtrl.sending().subscribe( obj -> {
-                if (obj instanceof HttpResponse) {
-                    final HttpResponse response = (HttpResponse)obj;
-                    final Cookie cookie = new DefaultCookie("hello", UUID.randomUUID().toString());
-                    cookie.setPath("/");
-                    final String cookieStr = ServerCookieEncoder.STRICT.encode(cookie);
-                    response.headers().set(HttpHeaderNames.SET_COOKIE, cookieStr);
+    public WithSubscriber<String> string1() {
+        final AtomicInteger cnt = new AtomicInteger(0);
+        return new WithSubscriber<String>() {
+
+            @Override
+            public String contentType() {
+                return "text/plain";
+            }
+
+            @Override
+            public void onSubscriber(final Subscriber<String> subscriber) {
+                if (cnt.get() < 10) {
+                    subscriber.onNext(cnt.addAndGet(1) + ", hello");
+                } else {
+                    subscriber.onCompleted();
                 }
-            });
-        } else {
-            LOG.info("Cookie (hello) has set, value {}", hello);
-        }
-        return "world";
+            }
+
+            @Override
+            public Action2<String, OutputStream> output() {
+                return (s, out) -> {
+                    try {
+                        out.write(s.getBytes(Charsets.UTF_8));
+                    } catch (final IOException e) {
+                    }
+                };
+            }};
     }
 
     @Path("basic/testcookie2")
