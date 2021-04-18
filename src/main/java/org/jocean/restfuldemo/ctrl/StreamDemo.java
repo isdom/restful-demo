e746f7e581756acb7df224908f80e99053b36358
ming.ma <isdom@qq.com>
2021-04-18 20:41:14 +0800

    实例流式无限响应, 每次等待1秒后，返回一个响应片段

--- src/main/java/org/jocean/restfuldemo/ctrl/StreamDemo.java
+++ src/main/java/org/jocean/restfuldemo/ctrl/StreamDemo.java
@@ -2,18 +2,12 @@ package org.jocean.restfuldemo.ctrl;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.ws.rs.CookieParam;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 
-import org.jocean.http.WriteCtrl;
 import org.jocean.idiom.ExceptionUtils;
-import org.jocean.svr.MutableResponseBean;
-import org.jocean.svr.ResponseUtil;
 import org.jocean.svr.WithSubscriber;
 import org.jocean.svr.annotation.HandleError;
 import org.jocean.svr.annotation.OnError;
@@ -24,11 +18,7 @@ import org.springframework.stereotype.Controller;
 
 import com.google.common.base.Charsets;
 
-import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.cookie.Cookie;
-import io.netty.handler.codec.http.cookie.DefaultCookie;
-import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action2;
@@ -80,167 +70,34 @@ public class StreamDemo {
             }};
     }
 
-    @Path("basic/testcookie2")
+    @Path("stream/endless")
     @OnError({
         "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
         ,"this.handleAllError"
         })
-    public Object testcookie2(@CookieParam("hello") final String hello) {
-        if (null == hello || hello.isEmpty()) {
-            final MutableResponseBean resp = ResponseUtil.response();
+    public WithSubscriber<String> endless() {
+        final AtomicInteger cnt = new AtomicInteger(0);
+        return new WithSubscriber<String>() {
 
-            final Cookie cookie = new DefaultCookie("hello", UUID.randomUUID().toString());
-            cookie.setPath("/");
-            final String cookieStr = ServerCookieEncoder.STRICT.encode(cookie);
+            @Override
+            public String contentType() {
+                return "text/plain";
+            }
 
-            resp.setStatus(200);
-            resp.withHeader().setHeader(HttpHeaderNames.SET_COOKIE.toString(), cookieStr);
+            @Override
+            public void onSubscriber(final Subscriber<String> subscriber) {
+                Observable.timer(1, TimeUnit.SECONDS).subscribe(any ->  subscriber.onNext(cnt.addAndGet(1) + ", hello"));
+            }
 
-            return resp;
-        } else {
-            LOG.info("Cookie (hello) has set, value {}", hello);
-            return "world";
-        }
+            @Override
+            public Action2<String, OutputStream> output() {
+                return (s, out) -> {
+                    try {
+                        out.write(s.getBytes(Charsets.UTF_8));
+                    } catch (final IOException e) {
+                    }
+                };
+            }};
     }
 
-    static class Response {
-        @HeaderParam("set-cookie")
-        String cookie;
-    }
-
-    @Path("basic/testcookie3")
-    @OnError({
-        "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
-        ,"this.handleAllError"
-        })
-    public Object testcookie3(@CookieParam("hello") final String hello) {
-        if (null == hello || hello.isEmpty()) {
-            final Response resp = new Response();
-
-            final Cookie cookie = new DefaultCookie("hello", UUID.randomUUID().toString());
-            cookie.setPath("/");
-            resp.cookie = ServerCookieEncoder.STRICT.encode(cookie);
-
-            return resp;
-        } else {
-            LOG.info("Cookie (hello) has set, value {}", hello);
-            return "world";
-        }
-    }
-
-    static class Response2 {
-        @CookieParam("hello")
-        String cookie;
-    }
-
-    @Path("basic/testcookie4")
-    @OnError({
-        "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
-        ,"this.handleAllError"
-        })
-    public Object testcookie4(@CookieParam("hello") final String hello) {
-        if (null == hello || hello.isEmpty()) {
-            final Response2 resp = new Response2();
-
-            resp.cookie = UUID.randomUUID().toString();
-
-            return resp;
-        } else {
-            LOG.info("Cookie (hello) has set, value {}", hello);
-            return "world";
-        }
-    }
-
-    static class Response3 {
-        @CookieParam("cookie1")
-        String cookie1;
-
-        @CookieParam("cookie2")
-        String cookie2;
-    }
-
-    @Path("basic/testcookie5")
-    @OnError({
-        "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
-        ,"this.handleAllError"
-        })
-    public Object testcookie5(
-            @CookieParam("cookie1") final String cookie1,
-            @CookieParam("cookie2") final String cookie2) {
-
-        final Response3 resp = new Response3();
-
-        if (null == cookie1 || cookie1.isEmpty()) {
-            resp.cookie1 = UUID.randomUUID().toString();
-        } else {
-            LOG.info("Cookie (cookie1) has set, value {}", cookie1);
-        }
-
-        if (null == cookie2 || cookie2.isEmpty()) {
-            resp.cookie2 = UUID.randomUUID().toString();
-        } else {
-            LOG.info("Cookie (cookie2) has set, value {}", cookie2);
-        }
-
-        return resp;
-    }
-
-    static class Response4 {
-        @CookieParam("cookie1")
-        String cookie1;
-
-        @HeaderParam("set-cookie")
-        String cookie2;
-    }
-
-    @Path("basic/testcookie6")
-    @OnError({
-        "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
-        ,"this.handleAllError"
-        })
-    public Object testcookie6(
-            @CookieParam("cookie1") final String cookie1,
-            @CookieParam("cookie2") final String cookie2) {
-
-        final Response4 resp = new Response4();
-
-        if (null == cookie1 || cookie1.isEmpty()) {
-            resp.cookie1 = UUID.randomUUID().toString();
-        } else {
-            LOG.info("Cookie (cookie1) has set, value {}", cookie1);
-        }
-
-        if (null == cookie2 || cookie2.isEmpty()) {
-            final Cookie cookie = new DefaultCookie("cookie2", UUID.randomUUID().toString());
-            cookie.setPath("/");
-            resp.cookie2 = ServerCookieEncoder.STRICT.encode(cookie);
-        } else {
-            LOG.info("Cookie (cookie2) has set, value {}", cookie2);
-        }
-
-        return resp;
-    }
-
-    // test with cmd:
-    // wget -q -O - http://api.2gdt.com/newrest/basic/stream
-    @Path("basic/stream")
-    @OnError({
-        "org.jocean.restfuldemo.ctrl.ErrorHandler.handleException"
-        ,"this.handleAllError"
-        })
-    public Observable<String> teststream(final WriteCtrl ctrl) {
-        ctrl.setFlushPerWrite(true);
-
-        return Observable.unsafeCreate(subscriber -> {
-            subscriber.onNext("hello,");
-
-            Observable.timer(10, TimeUnit.SECONDS).subscribe(any -> {
-                subscriber.onNext("world");
-                Observable.timer(10, TimeUnit.SECONDS).subscribe(any2 -> {
-                    subscriber.onNext("!");
-                    subscriber.onCompleted();
-                });
-            });
-        });
-    }
 }
