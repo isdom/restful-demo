c2fc72e4a6cef68ecc42bf25cf6449b345ae4c0b
ming.ma <isdom@qq.com>
2021-08-11 19:21:43 +0800

    改进 /newrest/wx/img_sec_check 实现: 从全 byte[] 拼接 到 文件部分直接使用 上传的 流式 MessageBody

--- src/main/java/org/jocean/restfuldemo/ctrl/WXDemo.java
+++ src/main/java/org/jocean/restfuldemo/ctrl/WXDemo.java
@@ -1,18 +1,15 @@
 package org.jocean.restfuldemo.ctrl;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-import org.jocean.http.ContentEncoder;
+import org.jocean.http.ByteBufSlice;
 import org.jocean.http.MessageBody;
 import org.jocean.http.RpcExecutor;
-import org.jocean.idiom.DisposableWrapper;
 import org.jocean.idiom.ExceptionUtils;
-import org.jocean.netty.util.BufsInputStream;
+import org.jocean.svr.ByteBufSliceUtil;
 import org.jocean.svr.annotation.HandleError;
 import org.jocean.wechat.WXProtocol;
 import org.slf4j.Logger;
@@ -21,14 +18,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.ByteStreams;
-import com.google.common.primitives.Bytes;
 
-import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.EmptyHttpHeaders;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpRequest;
 import rx.Observable;
-import rx.functions.Action2;
 
 
 @Path("/newrest/")
@@ -51,30 +46,14 @@ public class WXDemo {
             @QueryParam("name") final String name,
             @QueryParam("boundary") final String boundary,
             @QueryParam("mime") final String mime,
+            @HeaderParam("content-length") final int contentLength,
             final Observable<MessageBody> getbody,
             final RpcExecutor rpc
             ) {
-        final BufsInputStream<DisposableWrapper<? extends ByteBuf>> is =
-                new BufsInputStream<>(dwb -> dwb.unwrap(), dwb -> dwb.dispose());
+//        final BufsInputStream<DisposableWrapper<? extends ByteBuf>> is =
+//                new BufsInputStream<>(dwb -> dwb.unwrap(), dwb -> dwb.dispose());
 
-        return getbody.flatMap(body -> body.content().doOnNext(bbs -> {
-                    try {
-                        is.appendIterable(bbs.element());
-                    } finally {
-                        bbs.step();
-                    }
-                })).flatMap(bbs -> Observable.empty(), e -> Observable.error(e), () -> {
-                    is.markEOS();
-                    byte[] content = new byte[0];
-                    try {
-                        content = ByteStreams.toByteArray(is);
-                    } catch (final IOException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-
-//                    final String contentType = "multipart/form-data; boundary=" + boundary;
-
+        return getbody.flatMap(body -> {
                     final StringBuilder sb = new StringBuilder();
                     sb.append("--");
                     sb.append(boundary);
@@ -84,31 +63,64 @@ public class WXDemo {
                     sb.append("Content-Type: " + mime);
                     sb.append("\r\n\r\n");
 
-                    final byte[] body = Bytes.concat(sb.toString().getBytes(Charsets.UTF_8), content, ("\r\n--" + boundary + "--\r\n").getBytes(Charsets.UTF_8));
+                    final byte[] prefix = sb.toString().getBytes(Charsets.UTF_8);
+                    final byte[] suffix = ("\r\n--" + boundary + "--\r\n").getBytes(Charsets.UTF_8);
 
-                    final ContentEncoder to_formdata = new ContentEncoder() {
-                        @Override
-                        public String contentType() {
-                            return "multipart/form-data; boundary=" + boundary;
-                        }
-                        @Override
-                        public Action2<Object, OutputStream> encoder() {
-                            return (bean, os) -> {
-                                final byte[] bytes = (byte[])bean;
-                                try {
-                                    os.write(bytes);
-                                } catch (final IOException e) {
-                                    e.printStackTrace();
-                                }
-                            };
-                        }};
+//                    final ContentEncoder to_formdata = new ContentEncoder() {
+//                        @Override
+//                        public String contentType() {
+//                            return "multipart/form-data; boundary=" + boundary;
+//                        }
+//                        @Override
+//                        public Action2<Object, OutputStream> encoder() {
+//                            return (bean, os) -> {
+//                                final byte[] bytes = (byte[])bean;
+//                                try {
+//                                    os.write(bytes);
+//                                } catch (final IOException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            };
+//                        }};
+
+//                    DefaultHttpHeaders dhh = new DefaultHttpHeaders();
+
+//                    dhh.set("Host", host);
+//                    dhh.set("User-Agent", "curl/7.64.1");
+//                    dhh.set("Accept", "*/*");
+//                    dhh.set("Content-Length", prefix.length + contentLength + suffix.length);
+//                    dhh.set("Content-Type", contentType);
+//                    dhh.set("Expect", "100-continue");
 
                     return rpc.submit(inters -> inters.flatMap(interact ->
                             interact.method(HttpMethod.POST)
                             .uri("https://api.weixin.qq.com")
                             .path("/wxa/img_sec_check")
                             .paramAsQuery("access_token", access_token)
-                            .body(body, to_formdata)
+                            .body(Observable.<MessageBody>just(new MessageBody() {
+
+                                @Override
+                                public HttpHeaders headers() {
+                                    return EmptyHttpHeaders.INSTANCE;
+                                }
+
+                                @Override
+                                public String contentType() {
+                                    return "multipart/form-data; boundary=" + boundary;
+                                }
+
+                                @Override
+                                public int contentLength() {
+                                    return prefix.length + contentLength + suffix.length;
+                                }
+
+                                @Override
+                                public Observable<? extends ByteBufSlice> content() {
+                                    return Observable.just(ByteBufSliceUtil.wrappedSlice(prefix))
+                                            .concatWith(body.content())
+                                            .concatWith(Observable.just(ByteBufSliceUtil.wrappedSlice(suffix)));
+                                }}))
+//                            .body(body, to_formdata)
                             .responseAs(WXProtocol.WXAPIResponse.class) ));
                 });
     }
