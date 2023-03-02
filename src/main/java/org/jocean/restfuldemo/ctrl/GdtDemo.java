8cda31f6bf2f88ce9d818996ad239a95476890aa
ming.ma <isdom@qq.com>
2023-03-02 13:25:53 +0800

    demo gdt session spi usage

---
+++ src/main/java/org/jocean/restfuldemo/ctrl/GdtDemo.java
@@ -0,0 +1,32 @@
+package org.jocean.restfuldemo.ctrl;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import org.jocean.svr.annotation.RpcFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+
+import gdt.session.SessionSPI;
+import rx.Observable;
+
+
+@Path("/newrest/")
+@Controller
+@Scope("prototype")
+public class GdtDemo {
+    private static final Logger LOG = LoggerFactory.getLogger(GdtDemo.class);
+
+    @RpcFacade
+    SessionSPI sessionspi;
+    
+    @Path("gdt/sid")
+    public Observable<String> ask(@QueryParam("sid") final String sid) {
+    	LOG.info("sid: {}", sid);
+    	
+    	return sessionspi.getBootRequest().sessionid(sid).call().map(bootreq -> bootreq.toString()).doOnNext( req -> LOG.info("req: {}", req));
+    }
+}
