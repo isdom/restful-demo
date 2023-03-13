e2bfd2004f3de74604d0ae25f5aa4ee68bf4cab3
ming.ma <isdom@qq.com>
2023-03-13 21:26:58 +0800

    adjust code format

--- src/main/java/org/jocean/restfuldemo/ctrl/ChatgptDemo.java
+++ src/main/java/org/jocean/restfuldemo/ctrl/ChatgptDemo.java
@@ -97,7 +97,9 @@ public class ChatgptDemo {
                         .call())
                 .map( completion -> {
                       final ChatChoice[] choices = completion.getChoices();
-                      if (choices.length >= 1 && choices[0].getMessage() != null && choices[0].getMessage().getContent() != null) {
+                      if (choices.length >= 1
+                		  && choices[0].getMessage() != null 
+                		  && choices[0].getMessage().getContent() != null) {
                           final String answer = "(" + choices[0].getMessage().getRole() + "):" + choices[0].getMessage().getContent()
                                   + "/" + choices[0].getIndex() + "/" + choices[0].getFinish_reason()
                                   + "/" + "{prompt_tokens:" + completion.getUsage().getPrompt_tokens()
